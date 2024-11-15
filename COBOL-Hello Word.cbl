432b47c72f591df0ab70f688e81ad8beafbc4779
Esthapaful <guilherme.mendeleck@gmail.com>
2024-11-14 23:48:56 -0300

    Create COBOL-Hello Word.cbl

---
+++ COBOL-Hello Word.cbl
@@ -0,0 +1,25 @@
+      *****************************************************************
+      * DATA     :  14/11/2024
+      * AUTOR    : GUILHERME MENDELECK
+      * OBJETIVO : PROGRAMA HELLO WORLD
+      * CPD      : INEFE
+      *****************************************************************
+       IDENTIFICATION DIVISION.
+      *
+       PROGRAM-ID. GMEN0001.
+       AUTHOR. GUILHERME MENDELECK.
+       DATE-WRITTEN. 14/11/24 @ 23:00:00.
+       DATE-COMPILED. 2024-11-14.
+      *
+       ENVIRONMENT DIVISION.
+      *
+      ***
+       DATA DIVISION.
+      *-------------------------------------
+       PROCEDURE DIVISION.
+      *
+             DISPLAY ' HELLO WORLD'.
+             DISPLAY ' MEU PRIMEIRO PROGRAMA COBOL'.
+      *
+             STOP RUN.
+       END PROGRAM GMEN0001.
